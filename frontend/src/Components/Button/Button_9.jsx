30303ee0da9428e5c7847f64f46381e028bf3af8
Thanh Phong <95267305+Phong0605@users.noreply.github.com>
2023-12-10 21:27:13 +0700

    Add files via upload

---
+++ frontend/src/Components/Button/Button_9.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Buttons.css';
+
+/* Button 9: BUTTON WHITE*/
+const Button_9 = () => {
+    return (
+        <div>
+            <button class="button_9">
+                <div class="child-box_9"></div>
+                <span>BUTTON</span>
+            </button>
+        </div>
+    );
+}
+
+export default Button_9;
