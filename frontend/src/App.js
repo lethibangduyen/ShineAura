b154570145a66c93bed91b25f37d9bd0cd074733
Lê Thị Băng Duyên <duyenltb21411@st.uel.edu.vn>
2023-12-11 14:15:41 +0700

    homepage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,10 @@
 
 import './App.css';
-import Navbar from './Components/Navbar/Navbar';
-import Footer from './Components/Footer/Footer';
+import Homepage from './Pages/Homepage/Homepage';
 function App() {
   return (
     <div className="app">
-    <Navbar />
-    
-    <Footer />
+    <Homepage />
   </div>
   );
 }
