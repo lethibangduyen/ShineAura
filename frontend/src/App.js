f8cbd9ddbf309c0143fe1b0af56dd96f13a616f9
Khánh Gia <khanhnlg21411@st.uel.edu.vn>
2023-12-14 17:43:10 +0700

    Policy pages and cover, json file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,24 @@
-
 import './App.css';
-import Homepage from './Pages/Homepage/Homepage';
+import Navbar from './Components/Navbar/Navbar';
+import Footer from './Components/Footer/Footer';
+// import Homepage from './Pages/Homepage/Homepage';
+import Policy from './Pages/Policy/Policy'; // Đảm bảo bạn import trang Policy từ đúng đường dẫn
+
+// import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 function App() {
   return (
     <div className="app">
-    <Homepage />
-  </div>
+
+      <Navbar />
+      <Policy/>
+
+      {/* <Route path="/" element={<Homepage />} /> */}
+      {/* <Route path="/Policy" element={<Policy />} /> */}
+
+      <Footer />
+
+    </div>
   );
 }
 
--- frontend/src/Pages/Homepage/Homepage.jsx
+++ frontend/src/Pages/Homepage/Homepage.jsx
@@ -23,47 +23,47 @@ const Homepage = () => {
           in experiencing and expressing your unique beauty through the
           limitless choices at ShineAura.
         </div>
+      </div>
+      <div className="wireframe-3">
+        <div className="products">PRODUCTS</div>
+        <div className="at-shineaura-we">
+          At ShineAura, we take pride in offering a diverse collection of beauty
+          products from world-renowned brands. Embrace and express your individual
+          beauty with limitless choices from ShineAura.
         </div>
-        <div className="wireframe-3">
-          <div className="products">PRODUCTS</div>
-          <div className="at-shineaura-we">
-            At ShineAura, we take pride in offering a diverse collection of beauty
-            products from world-renowned brands. Embrace and express your individual
-            beauty with limitless choices from ShineAura.
-          </div>
-          <div className="rectangle-parent">
-            <div className="group-child" />
-            <i className="next bi bi-chevron-compact-left"></i>
-          </div>
-          <div className="rectangle-group">
-            <div className="group-child" />
-            <i class="next bi bi-chevron-compact-right"></i>
-          </div>
-          <div className="product-card-parent">
-            <div className="product-card">
-              <div className="rectangle-container">
-                <div className="group-inner" />
-                <div className="ce-hazy-lip">3CE HAZY LIP CLAY</div>
-                <i className="usd">$20 USD</i>
-                <div className='group-icon'>
-                  <i className="bi bi-heart"></i>
-                  <i className="bi bi-cart-plus"></i>
-                  <i className="bi bi-check2-square"></i>
-                </div>
+        <div className="rectangle-parent">
+          <div className="group-child" />
+          <i className="next bi bi-chevron-compact-left"></i>
+        </div>
+        <div className="rectangle-group">
+          <div className="group-child" />
+          <i className="next bi bi-chevron-compact-right"></i>
+        </div>
+        <div className="product-card-parent">
+          <div className="product-card">
+            <div className="rectangle-container">
+              <div className="group-inner" />
+              <div className="ce-hazy-lip">3CE HAZY LIP CLAY</div>
+              <i className="usd">$20 USD</i>
+              <div className='group-icon'>
+                <i className="bi bi-heart"></i>
+                <i className="bi bi-cart-plus"></i>
+                <i className="bi bi-check2-square"></i>
               </div>
-              <div className="product-card-child" />
-              <img className="image-52-icon" alt="" src="../../Components/Assets/Media/Product/product.png" />
-              <img className="product-card-item" alt="" src="../../Components/Assets/color.png" />
             </div>
-          </div>
-          <div className="underline-button">
-            <div className="button-parent">
-              <div className="button">See more</div>
-              <img className="icon" alt="" src="/icon.svg" />
-            </div>
+            <div className="product-card-child" />
+            <img className="image-52-icon" alt="" src="../../Components/Assets/Media/Product/product.png" />
+            <img className="product-card-item" alt="" src="../../Components/Assets/color.png" />
           </div>
         </div>
-      
+        <div className="underline-button">
+          <div className="button-parent">
+            <div className="button">See more</div>
+            <img className="icon" alt="" src="/icon.svg" />
+          </div>
+        </div>
+      </div>
+
       <Footer />
     </div>
   );
