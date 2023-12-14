7b0edf84e7ebf8a20c64cbf118f3ea31374ef20a
Phạm Leos <baoph21411@st.uel.edu.vn>
2023-12-14 19:02:33 +0700

    Add Product Card + SASS intergration

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+<<<<<<< Updated upstream
 // import Navbar from './Components/Navbar/Navbar';
 // import Footer from './Components/Footer/Footer';
 import Homepage from './Pages/Homepage/Homepage';
@@ -7,6 +8,17 @@ function App() {
     <div className="app">
    <Homepage/>
   </div>
+=======
+// import Homepage from './Pages/Homepage/Homepage';
+// import AboutUsPage from './pages/AboutUs/AboutUsPage';
+import ProductCard from './Components/Product Card/product-card';
+
+function App() {
+  return (
+    <div className="app">
+      <ProductCard />
+    </div>
+>>>>>>> Stashed changes
   );
 }
 
