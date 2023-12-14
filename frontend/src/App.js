ff8613ffd8acb1a086cfdaff87326e8fadc1de27
Phạm Leos <baoph21411@st.uel.edu.vn>
2023-12-14 10:00:33 +0700

    Update About Us Page
    Our Value added.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
-import Homepage from './Pages/Homepage/Homepage';
+// import Homepage from './Pages/Homepage/Homepage';
+import AboutUsPage from './pages/AboutUs/AboutUsPage';
+
 function App() {
   return (
     <div className="app">
-    <Homepage />
-  </div>
+      <AboutUsPage></AboutUsPage>
+    </div>
   );
 }
 
