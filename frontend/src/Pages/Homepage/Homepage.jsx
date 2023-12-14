60c73f208fafebdcae0b411acc472e378cdb85e5
Lê Thị Băng Duyên <duyenltb21411@st.uel.edu.vn>
2023-12-14 17:59:30 +0700

    update

--- frontend/src/Pages/Homepage/Homepage.jsx
+++ frontend/src/Pages/Homepage/Homepage.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import NavBar from '../../Components/Navbar/Navbar';
 import Footer from '../../Components/Footer/Footer';
-
 import Pic1 from '../../Components/Assets/Media/Homepage/Hero_1.png';
 import Pic2 from '../../Components/Assets/Media/Homepage/Hero_2.png';
 import Pic3 from '../../Components/Assets/Media/Homepage/Hero_3.png';
@@ -39,6 +38,10 @@ const Homepage = () => {
         <div className="text-product">
           <div className="products h1">PRODUCTS</div>
           <div className="at-shineaura-we body">
+            At ShineAura, we take pride in offering a diverse collection of beauty
+            products from world-renowned brands. Embrace and express your individual
+            beauty with limitless choices from ShineAura.
+          </div>
         </div>
         <div className="product-parent">
           <div className="buttons">
@@ -133,40 +136,6 @@ const Homepage = () => {
           </div>
         </div>
       </div>
-=======
-          <div className="rectangle-parent">
-            <div className="group-child" />
-            <i className="bi bi-chevron-compact-left"></i>
-          </div>
-          <div className="rectangle-group">
-            <div className="group-child" />
-            <i class="bi bi-chevron-compact-right"></i>
-          </div>
-          <div className="product-card-parent">
-            <div className="product-card">
-              <div className="rectangle-container">
-                <div className="group-inner" />
-                <div className="ce-hazy-lip">3CE HAZY LIP CLAY</div>
-                <i className="usd">$20 USD</i>
-                <div className='group-icon'>
-                  <i className="bi bi-heart"></i>
-                  <i className="bi bi-cart-plus"></i>
-                  <i className="bi bi-check2-square"></i>
-                </div>
-              </div>
-              <div className="product-card-child" />
-              <img className="image-52-icon" alt="" src="../../Components/Assets/product.png" />
-              <img className="product-card-item" alt="" src="../../Components/Assets/color.png" />
-            </div>
-          </div>
-          <div className="underline-button">
-            <div className="button-parent">
-              <div className="button">See more</div>
-              <img className="icon" alt="" src="/icon.svg" />
-            </div>
-          </div>
-        </div>
-    
       <Footer />
     </div>
   );
