2cf929dcf1879c854e945cd02ef30552da953cf4
Thanh Phong <95267305+Phong0605@users.noreply.github.com>
2023-12-17 23:25:25 +0700

    Delete frontend/src/Components/Button/Button_sign_in.jsx

--- frontend/src/Components/Button/Button_sign_in.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import './Buttons.css';  
-const SignInButton = () => {
-    return (
-        <button className="sign-in-button">Sign In</button>
-    );
-}
-
-export default SignInButton;
