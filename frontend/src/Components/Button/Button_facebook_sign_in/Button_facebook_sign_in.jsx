5dc39d64aad6cafd6ea99250c8062c6074e244fe
Thanh Phong <95267305+Phong0605@users.noreply.github.com>
2023-12-17 23:32:52 +0700

    Add files via upload

---
+++ frontend/src/Components/Button/Button_facebook_sign_in/Button_facebook_sign_in.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Button_facebook_sign_in.css';  // Import the common styling
+
+const FacebookSignInButton = () => {
+    return (
+        <button className="facebook-sign-in-button">
+            <i className="bi bi-facebook"></i>Facebook
+        </button>
+    );
+}
+
+export default FacebookSignInButton;
