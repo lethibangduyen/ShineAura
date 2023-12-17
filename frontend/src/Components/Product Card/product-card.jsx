1ff7ed0bdea22dd0c5ab083dd24c2aa7768550ba
Phạm Leos <baoph21411@st.uel.edu.vn>
2023-12-17 23:11:34 +0700

    Update Product parameter in Product Card

--- frontend/src/Components/Product Card/product-card.jsx
+++ frontend/src/Components/Product Card/product-card.jsx
@@ -22,18 +22,20 @@ const ProductCard = ({ product }) => {
         <div className={containerClassName}>
             <div className='flex-col'>
                 <div className='flex-row prod-img-container'>
-                    <img src={ProductImage} alt="Product Image" className='prod-img'/>
+                    <img src={product.images[0]} alt="Product Image" className='prod-img'/>
                 </div>
                 <div className='flex-row prod-info bg-ivory'>
                     <div className='flex-col gap-sm left-bar'>
                         <div className='flex-col gap-2xs prod-n-pr'>
-                            <div className='prod-name'>3CE HAZY LIP CLAY</div>
-                            <div className='prod-price'>$20 USD</div>
+                            <div className='prod-name'>{product.product_name}</div>
+                            <div className='prod-price'>{product.price + ".000 đ"}</div>
                         </div>
                         <div className='flex-row gap-2xs color-vars'>
-                            <div className='color-var'></div>
-                            <div className='color-var'></div>
-                            <div className='color-var'></div>
+                            {product.variants.map((variant, index) => (
+                                <div key={index} className='color-var'>
+                                {variant}
+                                </div>
+                            ))}
                         </div>
                     </div>
                     <div className='flex-col gap-2xs icon-collection'>
@@ -46,22 +48,20 @@ const ProductCard = ({ product }) => {
             <div className='flex-col align-left prod-info-exp'>
                 <div className='flex-col gap-xs align-left'>
                     <div className='flex-col gap-2xs prod-n-pr-review align-left'>
-                        <p className='prod-collection'>COLLECTION NAME</p>
-                        <p className='prod-name'>3CE HAZY LIP CLAY</p>
+                        <p className='prod-brand'>{product.brand}</p>
+                        <p className='prod-name'>{product.name}</p>
                         <div className='flex-row prod-review gap-2xs'>
-                            <div className='review-stars flex-row'>
-                                <i className='bi bi-star square-icon'></i>
-                                <i className='bi bi-star square-icon'></i>
-                                <i className='bi bi-star square-icon'></i>
-                                <i className='bi bi-star square-icon'></i>
-                                <i className='bi bi-star square-icon'></i>
-                            </div>
+                            {product.tab_data.description.map((para, index) => (
+                                        <p key={index} className='para'>
+                                        {para}
+                                        </p>
+                            ))}
                             <p className='review-rate'>000 review</p>
                         </div>
                     </div>
-                    <div className='prod-price'>$9.99</div>
+                    <div className='prod-price'>{product.price}</div>
                     <div className='prod-des'>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus neque tellus, venenatis at tincidunt sed, fermentum eu nibh. Mauris aliquet feugiat ex imperdiet tristique. Quisque vehicula, turpis at accumsan accumsan.</p>
+                        <p></p>
                     </div>
                     <div className='flex-col gap-2xs align-left color-select'>
                         <p className='select-label'>Colors</p>
