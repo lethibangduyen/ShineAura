2f3faa9ad59e3135df50fc4eb543ca3f76ddf62f
Khánh Gia <khanhnlg21411@st.uel.edu.vn>
2023-12-17 21:41:43 +0700

    update payment page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 // import Signup from './Pages/Signup/Signup';
 // import Navbar from './Components/Navbar/Navbar';
 // import Homepage from './Pages/Homepage/Homepage';
+<<<<<<< Updated upstream
 import Signin from './Pages/Signin/Signin';
 import ForgotPassword from './Pages/ForgotPassword/ForgotPassword';
 function App() {
@@ -13,6 +14,25 @@ function App() {
         // <Login />
         <Signin />
     );
+=======
+import Payment from './Pages/Payment/Payment'; // Đảm bảo bạn import trang Policy từ đúng đường dẫn
+
+// import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="app">
+
+      <Navbar />
+      {/* <Policy/> */}
+      <Payment/>
+
+
+      {/* <Footer /> */}
+
+    </div>
+  );
+>>>>>>> Stashed changes
 }
 
 export default App;
