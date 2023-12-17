5e393eb7dd55aa59a4e0e2ed9fd61bec4a50c11a
Thanh Phong <95267305+Phong0605@users.noreply.github.com>
2023-12-17 23:31:54 +0700

    Add files via upload

---
+++ frontend/src/Components/Button/Button_blackfont_transBG/Button_blackfont_transBG.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Button_blackfont_transBG.css';
+
+/* Button 6: BUTTON BLACK FONT + TRANSPARENT BG*/
+const Button_blackfont_transBG = () => {
+    return (
+        <div>
+            <button className="button_6">
+                <div className="child-box_6"></div>
+                <span>BUTTON</span>
+            </button>
+        </div>
+    );
+}
+
+export default Button_blackfont_transBG;
