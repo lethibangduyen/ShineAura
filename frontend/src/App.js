c30368155b00856391b8e4fcb30c95ac64a26771
thanhloc81 <loc222003@gmail.com>
2023-12-19 08:57:59 +0700

    SignUp, Forgor Password fixed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,16 @@
 import './App.css';
+import ForgotPassword from './Pages/ForgotPassword/ForgotPassword';
 // import Navbar from './Components/Navbar/Navbar';
 // import Signin from './Pages/Signin/Signin';
 // import ForgotPassword from './Pages/ForgotPassword/ForgotPassword';
-// import Signup from './Pages/Signup/Signup';
-import AboutUsPage from './Pages/AboutUs/AboutUsPage';
+import Signup from './Pages/Signup/Signup';
+// import AboutUsPage from './Pages/AboutUs/AboutUsPage';
 
 function App() {
   return (
     <div className="app">
-      <AboutUsPage/>
+      <ForgotPassword/>
+      {/* <Signup/> */}
     </div>
   );
 }
