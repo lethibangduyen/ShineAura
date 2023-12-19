d8aa7dcf4451cd4ce6d7cca8bc61a389403432c3
Phạm Leos <baoph21411@st.uel.edu.vn>
2023-12-19 11:40:58 +0700

    Fix Product Card unreadable paremter keys

--- frontend/src/Components/Productcard/product-card.jsx
+++ frontend/src/Components/Productcard/product-card.jsx
@@ -4,10 +4,73 @@ import './product-card.scss'
 
 import 'bootstrap-icons/font/bootstrap-icons.css'
 
-import ProductImage from '../Assets/Media/Product/Product-Image.png'
 import { useState } from 'react'
 
-const ProductCard = ({ product }) => {
+const ProductCard = ({}) => {
+    const product = {
+        "product_url": "https://nuty.vn/trang-diem/trang-diem-mat/phan-mat/bang-phan-mat-co-ban-hang-ngay-nature-republic-blooming-mood-26g",
+        "product_id": "8806173475445",
+        "product_name": "Bảng Phấn Mắt Cơ Bản Hàng Ngày Nature Republic  Blooming Mood (2.6g) - #02 Rosy",
+        "product_type": "Phấn Mắt",
+        "price": 215,
+        "images": [
+          "https://nuty.vn/Data/Sites/1/Product/29149/z4891856347478_320f196722e4d537ffa3ada992b501f6.jpg",
+          "https://nuty.vn/Data/Sites/1/Product/29149/z4889599308399_5afc67fcd8ebc719c6083d9025e003ee.jpg",
+          "https://nuty.vn/Data/Sites/1/Product/29149/z4889599259992_08fd40eb2dd37456e561a9d34d19efa7.jpg",
+          "https://nuty.vn/Data/Sites/1/Product/29149/z4891856356887_c80433fa13fda02d464d5bb96373129c.jpg",
+          "https://nuty.vn/Data/Sites/1/Product/29149/z4891856347478_320f196722e4d537ffa3ada992b501f6.jpg",
+          "https://nuty.vn/Data/Sites/1/Product/29149/z4889599308399_5afc67fcd8ebc719c6083d9025e003ee.jpg",
+          "https://nuty.vn/Data/Sites/1/Product/29149/z4889599259992_08fd40eb2dd37456e561a9d34d19efa7.jpg",
+          "https://nuty.vn/Data/Sites/1/Product/29149/z4891856356887_c80433fa13fda02d464d5bb96373129c.jpg",
+          "https://nuty.vn/Data/Sites/1/Product/28902/z4773161730247_e4225ff89f547cc314fdfbb0916d6139.jpg",
+          "https://nuty.vn/Data/Sites/1/Product/28798/z4635400015624_0219c6adc7764dd7908e7e19afd09d4f.jpg",
+          "https://nuty.vn/Data/Sites/1/Product/28928/z4767129684998_da8913def5fc6119a02ca5b8a9b42a06.jpg"
+        ],
+        "variants": [
+          "#02 Rosy",
+          "#03 Cool Pink",
+          "#04 Coral"
+        ],
+        "brands": "Nature Republic",
+        "tab_data": {
+          "description": [
+            "Bảng phấn mắt Nature Republic Daily Basic Palette  với các gam màu cơ bản thích hợp cho lớp trang điểm hàng ngày của bạn.",
+            "Vui lòng xem trên bao bì",
+            "Bao gồm 4 ô màu: màu lì và màu nhũ",
+            "Màu 02 Rosy:  Tập hợp các gam màu nhẹ nhàng, lãng mạn.",
+            "Màu 03 Cool Pink:  Một bảng màu với các sắc hồng tươi tắn giúp tô điểm thêm sự đáng yêu cho khuôn mặt của bạn.",
+            "Màu 04 Coral:  Bảng phấn mắt với các gam màu cam giúp mang lại lớp trang điểm tươi mát, dễ thương cho khuôn mặt của bạn.",
+            "- Bảng phấn mắt gồm các màu cơ bản dùng cho trang điểm hàng ngày và cho cả những người mới bắt đầu trang điểm.",
+            "- Các màu lì có chất phấn mịn màng và độ bám chặt mang lại lớp trang điểm tươi tắn và không lem.",
+            "- Các màu nhũ với các hạt nhũ đa dạng về kích thước giúp đôi mắt trông hài hòa và tự nhiên",
+            "- Bước 1:  Dùng cọ tán màu sáng lên bầu mắt trên và dưới để làm đều màu bề mặt và vùng da quanh mắt.",
+            "- Bước 2:  Dùng cọ tán màu trung bình lên bầu mắt tới sát chân mi để tạo sự chuyển màu.",
+            "- Bước 3:  Tán màu đậm lên mí mắt và góc 1/3 đuôi mắt.",
+            "- Bước 4:  Thoa màu nhũ lên trên mí mắt hoặc khóe mắt trong để mắt long lanh hơn.",
+            "Xuất xứ: Hàn Quốc",
+            "Thương hiệu: Black Rouge"
+          ],
+          "ingredients": [
+            "THÀNH PHẦN:  \nVui lòng xem trên bao bì",
+            "Bao gồm 4 ô màu: màu lì và màu nhũ"
+          ],
+          "effects": [
+            "CÔNG DỤNG:",
+            "Màu 02 Rosy:  Tập hợp các gam màu nhẹ nhàng, lãng mạn.",
+            "Màu 03 Cool Pink:  Một bảng màu với các sắc hồng tươi tắn giúp tô điểm thêm sự đáng yêu cho khuôn mặt của bạn.",
+            "Màu 04 Coral:  Bảng phấn mắt với các gam màu cam giúp mang lại lớp trang điểm tươi mát, dễ thương cho khuôn mặt của bạn.",
+            "- Bảng phấn mắt gồm các màu cơ bản dùng cho trang điểm hàng ngày và cho cả những người mới bắt đầu trang điểm.",
+            "- Các màu lì có chất phấn mịn màng và độ bám chặt mang lại lớp trang điểm tươi tắn và không lem.",
+            "- Các màu nhũ với các hạt nhũ đa dạng về kích thước giúp đôi mắt trông hài hòa và tự nhiên"
+          ],
+          "instruction": [
+            "HƯỚNG DẪN SỬ DỤNG:",
+            "- Bước 1:  Dùng cọ tán màu sáng lên bầu mắt trên và dưới để làm đều màu bề mặt và vùng da quanh mắt.",
+            "- Bước 2:  Dùng cọ tán màu trung bình lên bầu mắt tới sát chân mi để tạo sự chuyển màu.",
+            "- Bước 3:  Tán màu đậm lên mí mắt và góc 1/3 đuôi mắt.",
+            "- Bước 4:  Thoa màu nhũ lên trên mí mắt hoặc khóe mắt trong để mắt long lanh hơn."
+          ]
+        }};
     const [isExpanded, setIsExpanded] = useState(false);
 
     const handleExpandClick = () => {
