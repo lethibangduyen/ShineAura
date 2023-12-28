1f39e6604a27a63ec1993bc91f73aea3a46dca9f
Lê Thị Băng Duyên <duyenltb21411@st.uel.edu.vn>
2023-12-28 12:17:47 +0700

    logic cart - db

---
+++ shine-aura-project/shine-aura-project/server/models/Product.js
@@ -0,0 +1,51 @@
+const productSchema = new mongoose.Schema({
+    _id: mongoose.Schema.Types.ObjectId, // Auto-generated unique identifier
+    productUrl: {
+      type: String,
+      required: true,
+      unique: true, // Ensure URL uniqueness for data integrity
+      trim: true, // Remove extra whitespace
+    },
+    productId: {
+      type: String,
+      required: true,
+      index: true, // Create an index for efficient querying
+    },
+    productName: {
+      type: String,
+      required: true,
+      trim: true, // Remove extra whitespace
+    },
+    productType: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    images: [
+      {
+        type: String,
+        required: true,
+        maxlength: 2000, // Limit string length for image URLs
+      },
+    ],
+    variants: [
+      {
+        // Define the structure for each variant (e.g., size, color, stock, etc.)
+      },
+    ],
+    brands: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    tabData: {
+      type: Object,
+    },
+  });
+  
+  module.exports = mongoose.model('Product', productSchema);
+  
