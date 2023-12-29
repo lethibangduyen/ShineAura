18960aa365ef638d21c1e2dd15fb53b8d0aa2eb2
Lê Thị Băng Duyên <duyenltb21411@st.uel.edu.vn>
2023-12-29 22:35:27 +0700

    Update Product.js

--- shine-aura-project/shine-aura-project/server/models/cart.js
+++ shine-aura-project/shine-aura-project/server/models/cart.js
@@ -2,8 +2,12 @@ const mongoose = require('mongoose');
 
 const cartSchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  productId: { type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true },
-  quantity: { type: Number, default: 1 },
+  items: [
+    {
+      productId: { type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true },
+      quantity: { type: Number, default: 1 },
+    }
+  ],
   createdAt: { type: Date, default: Date.now },
 });
 
