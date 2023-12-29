8384775b1b95fa69ddce78434403b7ed285e1051
Phạm Leos <103585589+leos0509@users.noreply.github.com>
2023-12-29 22:32:47 +0700

    Cart Backend Logic Update

--- shine-aura-project/shine-aura-project/server/models/Product.js
+++ shine-aura-project/shine-aura-project/server/models/Product.js
@@ -1,3 +1,5 @@
+const mongoose = require('mongoose');
+
 const productSchema = new mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId, // Auto-generated unique identifier
     productUrl: {
--- shine-aura-project/shine-aura-project/server/routes/productRoute.js
+++ shine-aura-project/shine-aura-project/server/routes/productRoute.js
@@ -1,9 +1,7 @@
-// routes/productRoute.js
 const express = require('express');
 const router = express.Router();
 const Product = require('../models/Product');
 
-// Định nghĩa route để lấy toàn bộ sản phẩm
 router.get('/products', async (req, res) => {
   try {
     const products = await Product.find({});
